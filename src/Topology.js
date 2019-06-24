76453c6465c84aa41cc35f93b3e16a3dca0a9094
Sylvain Afchain <safchain@gmail.com>
2019-06-24 13:33:32 +0200

    Transition ok

--- src/Topology.js
+++ src/Topology.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { hierarchy, tree } from 'd3-hierarchy'
 import { select } from 'd3-selection'
 import { linkVertical } from 'd3-shape'
+import { } from 'd3-transition'
 
 
 export class TopologyComponent extends Component {
@@ -26,6 +27,7 @@ export class TopologyComponent extends Component {
 
     createTree() {
         var treeRoot = {
+            id: "root",
             data: {
                 name: "root"
             },
@@ -33,6 +35,7 @@ export class TopologyComponent extends Component {
             children: []
         }
 
+        // return depth of the given layer
         var layerDepth = (node, layer, currDepth) => {
             if (node.layer > layer) {
                 return 0
@@ -42,7 +45,7 @@ export class TopologyComponent extends Component {
             if (node.children) {
                 for (let child of node.children) {
                     let depth = layerDepth(child, layer, currDepth + 1)
-                    if (depth >= maxDepth) {
+                    if (depth > maxDepth) {
                         maxDepth = depth
                     }
                 }
@@ -51,6 +54,19 @@ export class TopologyComponent extends Component {
             return maxDepth
         }
 
+        var clone = (node, parent) => {
+            var cloned = {id: node.id, data: node.data, layer: node.layer, children: []}
+            if (parent) {
+                cloned.parent = parent
+            }
+
+            for (let child of node.children) {
+                cloned.children.push(clone(child, cloned))
+            }
+            return cloned
+        }
+
+        // re-order tree to add placeholder node in order to separate layers
         var normalizeTreeDepth = (root, node, layer, currDepth) => {
             if (node.layer && node.layer > layer) {
                 return
@@ -58,12 +74,12 @@ export class TopologyComponent extends Component {
 
             if (node.layer > 0 && node.layer === layer && node.parent && node.parent.layer !== layer) {
                 var parentDepth = layerDepth(root, node.layer - 1, 0)
-               
+
                 var children =  node.parent.children
                 var index = children.indexOf(node)
 
                 while(currDepth <= parentDepth) {
-                    node = {children: [node]}
+                    node = {id: node.id + "-" + currDepth, parent: node.parent, children: [node]}
                     currDepth++
                 }
                 children[index] = node
@@ -82,46 +98,15 @@ export class TopologyComponent extends Component {
 
         var maxLayer = 0
 
-        var addChild = (parent, data, layer) => {
+        var addChild = (parent, id, data, layer) => {
             if (layer > maxLayer) {
                 maxLayer = layer
             }
 
-            var child = {parent: parent, layer: layer, data: data, children: []}
+            var child = {id: id, parent: parent, layer: layer, data: data, children: []}
             parent.children.push(child)
-
-            if (layer !== parent.layer) {
-                for (let i = 0; i <= maxLayer; i++) {
-                    normalizeTreeDepth(treeRoot, treeRoot, i, 0)
-                }
-            }
             return child
         }
-
-        var host = addChild(treeRoot, {name: "host 1"}, 1)
-        addChild(host, {name: "lo"}, 1)
-        addChild(host, {name: "eth0"}, 1)
-        addChild(host, {name: "br-int"}, 1)
-
-        var br = addChild(host, {name: "br-int"}, 2)
-        addChild(br, {name: "port1"}, 2)
-        addChild(br, {name: "port2"}, 2)
-        addChild(br, {name: "port3"}, 2)
-
-       var ns1 = addChild(host, {name: "ns1"}, 3)
-        addChild(ns1, {name: "lo"}, 3)
-        addChild(ns1, {name: "eth0"}, 3)
-
-        var ns2 = addChild(host, {name: "ns2"}, 3)
-        addChild(ns2, {name: "lo"}, 3)
-        addChild(ns2, {name: "eth0"}, 3)
-
-        var root = hierarchy(treeRoot);
-        this.tree(root)
-
-
-
-        console.log(treeRoot)
 
         var g = select(this.node)
             .append("g")
@@ -130,7 +115,7 @@ export class TopologyComponent extends Component {
             .attr("class", "nodes")
             .attr("transform",
                 "translate(" + this.margin.left + "," + this.margin.top + ")")
-        
+
         var gLinks = g.append("g")
             .attr("class", "links")
             .attr("fill", "none")
@@ -140,35 +125,80 @@ export class TopologyComponent extends Component {
             .attr("transform",
                 "translate(" + this.margin.left + "," + this.margin.top + ")")
 
-        var i = 0
+        var i = 0;
 
-        var link = gLinks.selectAll('g.link')
-            .data(root.links(), function (d) { return d.id || (d.id = ++i) })
-        link.enter()
-            .append('path')
-            .attr("d", linkVertical()
-            .x(d => d.x)
-            .y(d => d.y))
+        setInterval(() => {
+            if (i > 2) {
+                return
+            }
+            i++
 
-        var node = gNodes.selectAll('g.node')
-            .data(root.descendants(), function (d) { return d.id || (d.id = ++i) })
-        var nodeEnter = node.enter()
-            .filter(function (d) { return d.data.data })
-            .append('g')
-            .attr('class', 'node')
-            .attr("transform", d =>`translate(${d.x},${d.y})`)
+            var host = addChild(treeRoot, "host-" + i, {name: "host " + i}, 1)
+            addChild(host, "host-lo-" + i, {name: "lo"}, 1)
+            addChild(host, "host-ethO-" + i, {name: "eth0"}, 1)
+            addChild(host, "host-br-int-" + i, {name: "br-int"}, 1)
 
-        nodeEnter.append("circle")
-            .attr("fill", d => d.children ? "#555" : "#999")
-            .attr("r", 2.5)
+            var br = addChild(host, "br-int-" + i, {name: "br-int"}, 2)
+            addChild(br, "port1-" + i, {name: "port1"}, 2)
+            addChild(br, "port2-" + i, {name: "port2"}, 2)
+            addChild(br, "port3-" + i, {name: "port3"}, 2)
 
-        nodeEnter.append("text")
-            .attr("dy", ".35em")
-            .attr("y", function (d) { return d.children ? -20 : 20 })
-            .style("text-anchor", "middle")
-            .text(function (d) {
-                return d.data.data ? d.data.data.name : ""
-            })
+            var ns1 = addChild(host, "ns1-" + i, {name: "ns1"}, 3)
+            addChild(ns1, "ns1-lo-" + i, {name: "lo"}, 3)
+            addChild(ns1, "ns1-eth0-" + i, {name: "eth0"}, 3)
+
+            var ns2 = addChild(host, "ns2-" + i, {name: "ns2"}, 3)
+            addChild(ns2, "ns2-lo-" + i, {name: "lo"}, 3)
+            addChild(ns2, "ns2-eth0-0" + i, {name: "eth0"}, 3)
+
+            var normTreeRoot = clone(treeRoot)
+            for (let i = 0; i <= maxLayer; i++) {
+                normalizeTreeDepth(normTreeRoot, normTreeRoot, i, 0)
+            }
+
+            var root = hierarchy(normTreeRoot);
+            this.tree(root)
+
+            var link = gLinks.selectAll('path.link')
+                .data(root.links(), d => { return d.source.data.id + d.target.data.id })
+            link.enter()
+                .append('path')
+                .attr("class", "link")
+                .attr("d", linkVertical()
+                .x(d => d.x)
+                .y(d => d.y))
+            link.exit().remove()
+
+            link.transition()
+                .duration(500)
+                .attr("d", linkVertical()
+                    .x(d => d.x)
+                    .y(d => d.y))
+
+            var node = gNodes.selectAll('g.node')
+                .data(root.descendants(), d =>{ return d.data.id })
+            node.exit().remove()
+
+            var nodeEnter = node.enter()
+                .filter(function (d) { return d.data.data })
+                .append("g")
+                .attr("class", "node")
+                .attr("transform", d => `translate(${d.x},${d.y})`)
+
+            nodeEnter.append("circle")
+                .attr("fill", d => d.children ? "#555" : "#999")
+                .attr("r", 2.5)
+
+            nodeEnter.append("text")
+                .attr("dy", ".35em")
+                .attr("y", d => { return d.children ? -20 : 20 })
+                .style("text-anchor", "middle")
+                .text(d => { return d.data.data ? d.data.data.name : "" })
+
+            node.transition()
+                .duration(500)
+                .attr("transform", d => `translate(${d.x},${d.y})`)
+        }, 1000)
     }
 
     render() {
