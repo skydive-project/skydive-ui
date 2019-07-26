77248b0aa0f809e943ea95aa5bfcf60bdfbd75e8
Sylvain Afchain <safchain@gmail.com>
2019-07-27 01:42:16 +0200

    Layer rect OK

--- src/Topology.js
+++ src/Topology.js
@@ -4,9 +4,12 @@ import { select, event } from 'd3-selection'
 import { line, linkVertical, curveCardinalClosed } from 'd3-shape'
 import { } from 'd3-transition'
 import { zoom } from 'd3-zoom'
-import { polygonHull } from 'd3-polygon'
-import { schemeOranges } from 'd3-scale-chromatic'
+import { schemeOranges, schemeBlues } from 'd3-scale-chromatic'
 import { scaleOrdinal } from 'd3-scale'
+import { } from 'd3-selection-multi'
+
+var colorOranges = scaleOrdinal(schemeOranges[9])
+var colorBlues = scaleOrdinal(schemeBlues[9])
 
 export class TopologyComponent extends Component {
     constructor(props) {
@@ -15,8 +18,8 @@ export class TopologyComponent extends Component {
         this.width = 1200
         this.height = 1200
 
-        this.nodeWidth = 80
-        this.nodeHeight = 120
+        this.nodeWidth = 100
+        this.nodeHeight = 200
 
         this.tree = tree().nodeSize([this.nodeWidth, this.nodeHeight])
     }
@@ -131,7 +134,7 @@ export class TopologyComponent extends Component {
             })
         }
 
-        var svg = select(this.node);
+        var svg = select(this.node)
 
         var g = svg
             .call(zoom()
@@ -142,18 +145,20 @@ export class TopologyComponent extends Component {
         var gLayers = g.append("g")
             .attr("class", "layers")
             .attr("fill", "none")
+            .attr("fill-opacity", 0.2)
 
-        var gLinks = g.append("g")
+        var gHieraLinks = g.append("g")
             .attr("class", "links")
             .attr("fill", "none")
             .attr("stroke", "#555")
-            .attr("stroke-opacity", 0.5)
+            .attr("stroke-opacity", 0.9)
             .attr("stroke-width", 1)
 
         var gLayerLinks = g.append("g")
             .attr("class", "layer-links")
             .attr("fill", "none")
-            .attr("stroke", "#400")
+            .attr("stroke", "#c8293c")
+            .attr("stroke-width", 3)
 
         var gNodes = g.append("g")
             .attr("class", "nodes")
@@ -179,7 +184,7 @@ export class TopologyComponent extends Component {
                 d.data.state.expanded = true
             }
 
-            update();
+            update()
         }
 
         var update = () => {
@@ -210,11 +215,16 @@ export class TopologyComponent extends Component {
 
             var _layerNodes = (node, nodes) => {
                 if (!nodes) {
-                    nodes = []
+                    nodes = {}
                 }
 
-                if (node.data._node) {
-                    nodes.push(node)
+                if (node.data.layer) {
+                    let arr = nodes[node.data.layer]
+                    if (!arr) {
+                        nodes[node.data.layer] = arr = { id: node.data.layer, nodes: [node] }
+                    } else {
+                        arr.nodes.push(node)
+                    }
                 }
 
                 if (node.children) {
@@ -226,81 +236,97 @@ export class TopologyComponent extends Component {
                 return nodes
             }
 
-            var layerNodes = (node, nodes) => {
-                if (!nodes) {
-                    nodes = []
-                }
+            var layerNodes = (node) => {
+                return Object.values(_layerNodes(node, {}))
+            }
 
-                if (node.data.parent && node.data.parent.layer !== node.data.layer) {
-                    nodes.push({ id: node.data.id, nodes: _layerNodes(node) })
+            var boundingBox = (node, bb) => {
+                if (!bb) {
+                    bb = [node.x, node.x]
+                } else {
+                    if (bb[0] > node.x) {
+                        bb[0] = node.x
+                    }
+                    if (bb[1] < node.x) {
+                        bb[1] = node.x
+                    }
                 }
 
                 if (node.children) {
                     node.children.forEach(child => {
-                        layerNodes(child, nodes)
+                        boundingBox(child, bb)
                     })
                 }
 
-                return nodes
+                return bb
             }
 
-            var layerPoints = (node) => {
-                return node.nodes.map(n => [n.x, n.y])
-            }
+            var nodesRect = (nodes) => {
+                let node0 = nodes[0]
+                let nBB = [node0.y, node0.y]
 
-            var layerPath = (node) => {
-                let hull = polygonHull(layerPoints(node))
-                if (hull) {
-                    return "M" + hull.join("L") + "Z";
+                for (let node of nodes) {
+                    if (nBB[0] > node.y) {
+                        nBB[0] = node.y
+                    }
+                    if (nBB[1] < node.y) {
+                        nBB[1] = node.y
+                    }
                 }
-                return ""
+
+                let gBB = boundingBox(root)
+                const margin = 100
+
+                return {
+                    x: gBB[0] - margin,
+                    y: nBB[0] - margin,
+                    width: gBB[1] - gBB[0] + margin * 2,
+                    height: nBB[1] - nBB[0] + margin * 2
+                }
             }
 
-            var colors = scaleOrdinal(schemeOranges[9]);
+            var linker = linkVertical()
+                .x(d => d.x)
+                .y(d => d.y)
 
-            var layer = gLayers.selectAll("path.layer")
-                .data(layerNodes(root), d => d.id)
-            var layerEnter = layer.enter()
-                .append("path")
+            var layers = gLayers.selectAll('rect.layer')
+                .data(layerNodes(root))
+            var layersEnter = layers.enter()
+                .append('rect')
                 .attr("class", "layer")
-                .attr("d", layerPath)
-                .attr("stroke-width", "72px")
-                .attr("stroke-linejoin", "round")
-                .attr("stroke", (d, i) => colors(d.id))
-                .attr("fill", (d, i) => colors(d.id))
+                .attr("id", d => "toto" + d.id)
                 .style("opacity", 0)
-            layer.exit().remove()
+                .attr("stroke", "#000")
+                .attr("stroke-dasharray", "5,10")
+                .attr("fill", d => colorBlues(d.id))
+                .attrs(d => nodesRect(d.nodes))
+            layers.exit().remove()
 
-            layerEnter.transition()
+            layersEnter.transition()
                 .duration(500)
                 .style("opacity", 1)
 
-            layer.transition()
+            layers.transition()
                 .duration(500)
-                .style("opacity", 1)
-                .attr("d", layerPath)
+                .attrs(d => nodesRect(d.nodes))
 
-            var link = gLinks.selectAll('path.link')
+            var hieraLink = gHieraLinks.selectAll('path.link')
                 .data(root.links(), d => d.source.data.id + d.target.data.id)
-            var linkEnter = link.enter()
+            var hieraLinkEnter = hieraLink.enter()
                 .append('path')
                 .attr("class", "link")
                 .attr("stroke-dasharray", "5,10")
                 .style("opacity", 0)
-                .attr("d", linkVertical()
-                    .x(d => d.x)
-                    .y(d => d.y))
-            link.exit().remove()
+                .attr("d", linker)
+            hieraLink.exit().remove()
 
-            linkEnter.transition()
+            hieraLinkEnter.transition()
                 .duration(500)
                 .style("opacity", 1)
 
-            link.transition()
+            hieraLink.transition()
                 .duration(500)
-                .attr("d", linkVertical()
-                    .x(d => d.x)
-                    .y(d => d.y))
+                .attr("d", linker)
 
             var node = gNodes.selectAll('g.node')
                 .data(root.descendants(), d => d.data.id)
@@ -321,7 +347,21 @@ export class TopologyComponent extends Component {
                 .duration(500)
                 .style("opacity", 1)
 
-            const hexSize = 20
+            const hexSize = 30
+
+            var groupColors = (d) => {
+                return colorOranges(d.data._node.layer)
+            }
+
+            nodeEnter.append("circle")
+                .attr("r", hexSize + 14)
+                .attr("fill", "#fff")
+                .attr("stroke", groupColors)
+                .attr("stroke-width", 3)
+
+            nodeEnter.append("circle")
+                .attr("r", hexSize + 8)
+                .attr("fill", groupColors)
 
             var hexagon = (d, size) => {
                 var s32 = (Math.sqrt(3) / 2)
@@ -345,40 +385,58 @@ export class TopologyComponent extends Component {
                 .y(d => d.y)
                 .curve(curveCardinalClosed.tension(0.7))
 
+            var types = {
+                host: { color: "#114B5F", icon: "\uf109", text: "#eee" },
+                bridge: { color: "#1A936F", icon: "\uf1e0", text: "#eee" },
+                interface: { color: "#88D498", icon: "\uf120", text: "#444" },
+                netns: { color: "#C6DABF", icon: "\uf24d", text: "#444" },
+                port: { color: "#1A936F", icon: "\uf0e8", text: "#eee" },
+                unknown: { color: "#C9ADA7", icon: "\uf192", text: "#444" }
+            }
+
+            var type = (d) => {
+                return types[d.data._node.data.type] ? types[d.data._node.data.type] : types["unknown"]
+            }
+
             nodeEnter.append("path")
                 .attr("d", d => liner(hexagon(d, hexSize)))
                 .attr("stroke", "#455f6b")
-                .attr("fill", "#c3dde2")
+                .attr("fill", d => type(d).color)
                 .attr("stroke-dasharray", "5,5")
 
-            nodeEnter.append("circle")
-                .attr("fill", d => d.children ? "#555" : "#999")
-                .attr("r", 2.5)
+            nodeEnter.append("text")
+                .attr("class", "icon")
+                .style("font-size", "24px")
+                .attr("alignment-baseline", "middle")
+                .attr("fill", d => type(d).text)
+                .attr("text-anchor", "middle")
+                .style("font-family", "FontAwesome")
+                .text(d => type(d).icon)
 
             nodeEnter.append("text")
+                .style("font-size", "24px")
                 .attr("dy", ".35em")
-                .attr("y", d => d.children ? -30 : 30)
+                .attr("y", d => d.children ? -60 : 60)
                 .style("text-anchor", "middle")
                 .text(d => d.data._node.data ? d.data._node.data.name : "")
 
             var exco = nodeEnter
                 .filter(d => d.data._node.children.length > 0)
                 .append("g")
-            exco.on("click", expand);
+            exco.on("click", expand)
 
             exco.append("circle")
                 .attr("class", "collapse")
                 .attr("cx", hexSize - 5)
                 .attr("cy", hexSize - 5)
-                .attr("r", d => d.data._node.children.length ? 7 : 0)
+                .attr("r", d => d.data._node.children.length ? 10 : 0)
                 .attr("fill", "#48e448")
+                .attr("stroke", "#666")
 
             exco.append("text")
                 .attr("class", "text-collapse")
                 .attr("x", hexSize - 5)
                 .attr("y", hexSize - 1)
-                .attr("width", 20)
-                .attr("height", 20)
                 .style("font-size", "10px")
                 .attr("fill", "#fff")
                 .attr("text-anchor", "middle")
@@ -390,15 +448,21 @@ export class TopologyComponent extends Component {
                 .style("opacity", 1)
                 .attr("transform", d => `translate(${d.x},${d.y})`)
 
-            var layerLink = gLayerLinks.selectAll('path.link')
+            var layerLinker = linkVertical()
+                .x(d => holders[d.node.id].x)
+                .y(d => holders[d.node.id].y + d.dy)
+
+            var layerLink = gLayerLinks.selectAll('path.layer-link')
                 .data(visibleLayerLinks(), d => d.id)
             var layerLinkEnter = layerLink.enter()
                 .append('path')
-                .attr("class", "link")
+                .attr("class", "layer-link")
                 .style("opacity", 0)
-                .attr("d", linkVertical()
-                    .x(d => holders[d.id].x)
-                    .y(d => holders[d.id].y))
+                .attr('marker-start', "url(#square)")
+                .attr('marker-end', "url(#square)")
+                .attr("d", d => layerLinker(
+                    { source: { node: d.source, dy: 55 }, target: { node: d.target, dy: -55 } }
+                ))
             layerLink.exit().remove()
 
             layerLinkEnter.transition()
@@ -408,50 +472,60 @@ export class TopologyComponent extends Component {
             layerLink.transition()
                 .duration(500)
                 .style("opacity", 1)
-                .attr("d", linkVertical()
-                    .x(d => holders[d.id].x)
-                    .y(d => holders[d.id].y))
+                .attr("d", d => layerLinker(
+                    { source: { node: d.source, dy: 55 }, target: { node: d.target, dy: -55 } }
+                ))
         }
 
-        var i = 0;
+        var i = 0
         setInterval(() => {
-            if (i > 1) {
+            if (i > 2) {
                 return
             }
             i++
 
-            var host = addChild(treeRoot, "host-" + i, { name: "host " + i }, 1)
-            addChild(host, "host-lo-" + i, { name: "lo" }, 1)
-            addChild(host, "host-ethO-" + i, { name: "eth0" }, 1)
-            var hbr = addChild(host, "host-br-int-" + i, { name: "br-int" }, 1)
+            var host = addChild(treeRoot, "host-" + i, { name: "host " + i, type: "host" }, 1)
+            addChild(host, "host-lo-" + i, { name: "lo", type: "interface" }, 1)
+            addChild(host, "host-ethO-" + i, { name: "eth0", type: "interface" }, 1)
+            var hbr = addChild(host, "host-br-int-" + i, { name: "br-int", type: "interface" }, 1)
 
-            var br = addChild(host, "br-int-" + i, { name: "br-int" }, 2)
+            var br = addChild(host, "br-int-" + i, { name: "br-int", type: "bridge" }, 2)
 
             addLayerLink(hbr, br, { RelationType: "layer2" })
 
-            var port1 = addChild(br, "port1-" + i, { name: "port1" }, 2)
-            var port2 = addChild(br, "port2-" + i, { name: "port2" }, 2)
-            addChild(br, "port3-" + i, { name: "port3" }, 2)
+            var port1 = addChild(br, "port1-" + i, { name: "port1", type: "port" }, 2)
+            var port2 = addChild(br, "port2-" + i, { name: "port2", type: "port" }, 2)
+            var port3 = addChild(br, "port3-" + i, { name: "port3", type: "port" }, 2)
 
-            var ns1 = addChild(host, "ns1-" + i, { name: "ns1" }, 3)
-            addChild(ns1, "ns1-lo-" + i, { name: "lo" }, 3)
-            var eth0 = addChild(ns1, "ns1-eth0-" + i, { name: "eth0" }, 3)
+            addLayerLink(br, port1, { RelationType: "layer2" })
+            addLayerLink(br, port2, { RelationType: "layer2" })
+            addLayerLink(br, port3, { RelationType: "layer2" })
+
+            var ns1 = addChild(host, "ns1-" + i, { name: "ns1", type: "netns" }, 3)
+            addChild(ns1, "ns1-lo-" + i, { name: "lo", type: "interface" }, 3)
+            var eth0 = addChild(ns1, "ns1-eth0-" + i, { name: "eth0", type: "interface" }, 3)
 
             addLayerLink(port1, eth0, { RelationType: "layer2" })
 
-            var ns2 = addChild(host, "ns2-" + i, { name: "ns2" }, 3)
-            addChild(ns2, "ns2-lo-" + i, { name: "lo" }, 3)
-            var eth0 = addChild(ns2, "ns2-eth0-0" + i, { name: "eth0" }, 3)
+            var ns2 = addChild(host, "ns2-" + i, { name: "ns2", type: "netns" }, 3)
+            addChild(ns2, "ns2-lo-" + i, { name: "lo", type: "interface" }, 3)
+            var eth0 = addChild(ns2, "ns2-eth0-0" + i, { name: "eth0", type: "interface" }, 3)
 
             addLayerLink(port2, eth0, { RelationType: "layer2" })
 
             update(null, true)
-        }, 200)
+        }, 700)
     }
 
     render() {
         return (
-            <svg ref={node => this.node = node} width={1200} height={1200}></svg>
+            <svg ref={node => this.node = node} width={1200} height={1200}>
+                <defs>
+                    <marker id="square" viewBox="-5 -5 10 10" markerWidth="6" markerHeight="6" orient="auto">
+                        <path d="M 0,0 m -5,-5 L 5,-5 L 5,5 L -5,5 Z" fill="#c8293c"></path>
+                    </marker>
+                </defs>
+            </svg>
         )
     }
 }
