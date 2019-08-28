7250e7e0f5f21f4538727b0ddfb86f22c60b89f6
Sylvain Afchain <safchain@gmail.com>
2019-08-28 15:43:17 +0200

    Start convertion to typescript

--- webpack.config.js
+++ webpack.config.js
@@ -16,12 +16,13 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
+                test: /\.(t|j)sx?$/,
                 use: {
-                    loader: "babel-loader"
-                }
+                    loader: 'awesome-typescript-loader'
+                },
+		exclude: /node_modules/
             },
+ { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
             {
                 test: /\.css$/,
                 use: ["style-loader", "css-loader"]
